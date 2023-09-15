db3aa87748b4d4ad8ecf33309ab9b93d762ebde5
homeboy445 <akshitsan13@gmail.com>
2023-09-15 23:28:38 +0530

    Added the build folder

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.join(__dirname, "src/index.js"),
+  entry: path.join(__dirname, "../src/index.js"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     filename: "main.js",
-    library: "getFetchInitiator"
+    // library: "getFetchInitiator"
   },
   module: {
     rules: [
